d1494e667c17189976d21065eb4b46da4476d350
itReverie <brenda@itreverie.com>
2018-02-03 15:48:39 -0500

    Adding an onClick event to delete and adding Perf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListCoins from './components/ListCoins';
-
-const coins=[{id:1, name:'bitcoin', color:'#a2a2a2', url:''},
-{id:1, name:'bitcoin', color:'#a2a2a2', url:''},
-{id:1, name:'ethereum', color:'#a2a2a2', url:''},
-{id:1, name:'ajax', color:'#a2a2a2', url:''}];
-
+import Perf from 'react-addons-perf';
+window.Perf=Perf;
 class App extends Component {
+
+
+
+
+constructor(props) {
+  super(props);
+       this.state = {coins: [{id:1, name:'bitcoin', color:'#a2a2a2', url:''},
+       {id:2, name:'bitcoin', color:'#a2a2a2', url:''},
+       {id:3, name:'ethereum', color:'#a2a2a2', url:''},
+       {id:4, name:'ajax', color:'#a2a2a2', url:''},
+     {id:5, name:'A', color:'#a2a2a2', url:''},
+   {id:6, name:'B', color:'#a2a2a2', url:''},
+ {id:7, name:'C', color:'#a2a2a2', url:''},
+{id:8, name:'D', color:'#a2a2a2', url:''}]};
+}
+  _handleDelete(id){
+    this.setState(prevState => ({
+        coins: prevState.data.filter(el => el != id )
+    }));
+}
+
+componentDidMount() {
+  Perf.start();
+  Perf.stop();
+  Perf.printWas
+}
   render() {
     return <div className="App">
         <header className="App-header">
@@ -16,7 +38,7 @@ class App extends Component {
           </h1>
         </header>
         <p className="App-intro" />
-        <ListCoins  coins={coins}/>
+        <ListCoins  coins={this.state.coins}/>
       </div>;
   }
 }
--- src/components/ListCoins.js
+++ src/components/ListCoins.js
@@ -2,16 +2,39 @@ import React, { Component } from "react";
 import Coin from "./Coin";
 import PropTypes from 'prop-types';
 
+class ListCoins extends Component{
 
-class CoinsList extends Component{
+  constructor(props){
+        super(props);
+        this.state = {
+          coins: this.props.coins
+        }
+
+     }
+
+     delete(id){
+           this.setState(prevState => ({
+               coins: prevState.coins.filter(el => el.id !== id )
+           }));
+        }
+
+  componentWillMount(){
+      this.delete = this.delete.bind(this);
+  }
+
  render(){
-    return (<div>{this.props.coins.map(coin => <Coin name={coin.name}/>)}</div>);
+    return (<div>{this.state.coins.map(coin => <Coin
+      key={coin.id}
+      id={coin.id}
+      name={coin.name}
+      delete={this.delete}
+      />)}</div>);
  }
 }
 
-CoinsList.propTypes = {
-  coins: PropTypes.array.isRequired,
-  onClick: PropTypes.func.isRequired
+ListCoins.propTypes = {
+  coins: PropTypes.array.isRequired
+
 };
 
-  export default CoinsList;
+  export default ListCoins;
